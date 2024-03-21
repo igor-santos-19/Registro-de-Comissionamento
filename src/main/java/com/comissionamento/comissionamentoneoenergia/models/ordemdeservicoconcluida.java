2180a4e83a39c9abe3bf4f6717166712df08bc7c
Igor Santos <63085756+igor-santos-19@users.noreply.github.com>
2024-03-21 13:28:36 -0300

    Model: criada as 2 tabelas do banco sem instancia

---
+++ src/main/java/com/comissionamento/comissionamentoneoenergia/models/ordemdeservicoconcluida.java
@@ -0,0 +1,71 @@
+package com.comissionamento.comissionamentoneoenergia.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+
+// Cria a tabela Ordem De Serviço Conluida no BD
+
+@Entity 
+@Table(name ="ordemdeservicoconcluida")
+public class ordemdeservicoconcluida {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(columnDefinition = "integer", length = 255)
+    private Integer cliente;
+
+    @Column(columnDefinition = "integer", length = 255)
+    private Integer protocolo;
+
+    @Column(length = 255)
+    private String empresa;
+
+    @Column(length = 100)
+    private String uteperesponsavel;
+
+    @Column(length = 80)
+    private String motivo;
+
+    @Column(length = 20)
+    private String situacao;
+
+    @Column(length = 10)
+    private String orgaoexecutor;
+
+    @Column(length = 30)
+    private String localose; //Local Ordem De Serviço
+
+    @Column(name = "dataabertura")
+    @Temporal(TemporalType.DATE)
+    private Date dataabertura;
+
+    @Column(name = "datalimite")
+    @Temporal(TemporalType.DATE)
+    private Date datalimite;
+
+    @Column(name = "dataconclusao")
+    @Temporal(TemporalType.DATE)
+    private Date dataconclusao;
+
+    @Column(length = 3)
+    private String foradoprazo;
+
+    @Column(length = 255)
+    private Integer diasatraso;
+
+    @Column(length = 255)
+    private String termos;
+
+    @Column(name = "dataenvio")
+    @Temporal(TemporalType.DATE)
+    private Date dataenvio;
+}
